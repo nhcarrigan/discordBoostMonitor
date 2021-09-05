886765abe244d5c90914765a7281a25795c437ed
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-09-05 07:59:06 -0700

    feat: build initial bot prototype

---
+++ src/modules/manageRoles.ts
@@ -0,0 +1,28 @@
+import { GuildMember, PartialGuildMember } from "discord.js";
+
+import { colourRoles } from "../config/roles";
+
+/**
+ * Attaches to the memberUpdate event, and checks if the member is no longer boosting
+ * the server (based on the premiumSubscriberRole). If true, then removes any of the configured
+ * booster-only roles.
+ *
+ * @param {GuildMember | PartialGuildMember} old The state of the member before the update.
+ * @param {GuildMember} newMember The state of the member after the update.
+ */
+export const manageRoles = async (
+  old: GuildMember | PartialGuildMember,
+  newMember: GuildMember
+): Promise<void> => {
+  if (
+    old.roles.premiumSubscriberRole &&
+    !newMember.roles.premiumSubscriberRole
+  ) {
+    for (const roleId of colourRoles) {
+      const target = newMember.roles.cache.find((role) => role.id === roleId);
+      if (target) {
+        await newMember.roles.remove(target);
+      }
+    }
+  }
+};
